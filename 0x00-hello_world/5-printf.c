777b76730039dcd4dc2afb4ea5ead2b05a9cec7d
WatitwaJeff <watitwajeff@gmail.com>
2023-02-16 10:36:53 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase as it is
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
